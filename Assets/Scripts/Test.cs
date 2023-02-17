ac9f83e3408a86d525ca80c4d200df2b118424ec
phatbidaulung <107571396+phatbidaulung@users.noreply.github.com>
2023-02-17 23:01:55 +0700

    add

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -6,17 +6,68 @@ using UnityEngine.AI;
 
 public class Test : MonoBehaviour 
 {
-	[SerializeField] private GameObject _player;
-    [SerializeField] private NavMeshAgent _enemy;
+    
+    private Vector2 startTouchPosition;
+    private Vector2 currentPosition;
+    private Vector2 endTouchPosition;
 
-    private void Start() {
-        _player = GameObject.FindGameObjectWithTag("Player");
+    private bool stopTouch = false;
+    
+    public Text _text;
+    public float swipeRange;
+    public float tapRange;
+
+    private void Update() {
+        Swipe();
     }
-    private void FixedUpdate() {
-        Move();
-    }
-    private void Move()
+
+    private void Swipe()
     {
-        _enemy.SetDestination(_player.transform.position);       
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTouchPosition = Input.GetTouch(0).position;
+        }
+
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            currentPosition = Input.GetTouch(0).position;
+            Vector2 distance = currentPosition - startTouchPosition;
+
+            if(!stopTouch)
+            {
+                if(distance.x < -swipeRange)
+                {
+                    _text.text = "Left";
+                    stopTouch = true;
+                }
+                else if(distance.x > swipeRange)
+                {
+                    _text.text = "Right";
+                    stopTouch = true;
+                }
+                else if(distance.y > swipeRange)
+                {
+                    _text.text = "Up";
+                    stopTouch = true;
+                }
+                else if(distance.y < -swipeRange)
+                {
+                    _text.text = "Down";
+                    stopTouch = true;
+                }
+
+            }
+        }
+
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            stopTouch = false;
+            endTouchPosition = Input.GetTouch(0).position;
+            Vector2 distance = endTouchPosition - startTouchPosition;
+            if(Mathf.Abs(distance.x) < tapRange && Mathf.Abs(distance.y) < tapRange)
+            {
+                _text.text = "Tap";
+            }
+        }
     }
 }
