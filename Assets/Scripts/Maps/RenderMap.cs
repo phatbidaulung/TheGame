603f0b46bda868ac0b179606fe40ecb1e58d446f
phatbidaulung <107571396+phatbidaulung@users.noreply.github.com>
2023-02-24 18:05:09 +0700

    add module map

--- Assets/Scripts/Maps/RenderMap.cs
+++ Assets/Scripts/Maps/RenderMap.cs
@@ -11,11 +11,13 @@ public class RenderMap : MonoBehaviour
     [SerializeField] private int _numberOfModuleMap;
     private int _numeberMapOfList;
     private int _numberMap;
+    private float _lengthMap = 10f;
     private float _locationNewMap;
     private void Start() 
     {
+        _locationNewMap += _typeMap == ETypeMap.EndLessMap ? _lengthMap : 0f;
         CreatePoolMapModule();
-        CreateMap();
+        CreateMap();            
     }
     private void CreatePoolMapModule()
     {
@@ -32,30 +34,28 @@ public class RenderMap : MonoBehaviour
     }
     public void CreateNewMap()
     {
-        float lengthMap = 10f;
-        _locationNewMap += lengthMap;
 
         switch (_typeMap)
         {
             case ETypeMap.NormalMap:
                 if(_numberMap < _maps.Count)
                 {
-                    _mapsCreated.Add(_maps[_numberMap].Spawn(new Vector3(_locationNewMap, transform.position.y, transform.position.z)));
+                    _mapsCreated.Add(_maps[_numberMap].Spawn(new Vector3(_locationNewMap, _maps[_numberMap].transform.position.y, _maps[_numberMap].transform.position.z)));
                     _numberMap++;
                 }
                 break;
             case ETypeMap.EndLessMap:
-                _mapsCreated.Add(RandomMap().Spawn(new Vector3(_locationNewMap, transform.position.y, transform.position.z)));
+                _mapsCreated.Add(RandomMap().Spawn(new Vector3(_locationNewMap, RandomMap().transform.position.y, RandomMap().transform.position.z)));
                 break;
         }
+
+        _locationNewMap += _lengthMap;
     }
 
     public void RecycleMap()
     {
         _mapsCreated[_numeberMapOfList].Recycle();
         _numeberMapOfList++;
-        Debug.Log("Reee");
-        Debug.Log(_numeberMapOfList);
     }
     private GameObject RandomMap()
     {
