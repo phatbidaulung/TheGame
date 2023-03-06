9d15643a6d63c6c7efa2175b7eb3de7b645ae9b4
phatbidaulung <107571396+phatbidaulung@users.noreply.github.com>
2023-03-06 20:31:29 +0700

    fix animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,59 +23,70 @@ public class PlayerController : MonoBehaviour
         _nextPosition = transform.position;
         _nextRotation = Quaternion.Euler(0f, 0f, 0f);
     }
-    private void FixedUpdate() 
+    private void Update() 
     {
         if(Input.GetKeyDown(KeyCode.Space))
             _animatorPlayer.SetTrigger("Jump");
 
-        AddListenInput();
+        if(_animatorPlayer.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        Debug.LogWarning($"Is play");
+
 
         transform.position = Vector3.MoveTowards(transform.position, new Vector3(_nextPosition.x , transform.position.y, _nextPosition.z), _speed);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, _nextRotation, _speedRT);
+        
+        AddListenInput();
+    }
+    private void FixedUpdate() {
         
     }
     private void Movement(EMovement typeMove)
     {
         
         _animatorPlayer.SetTrigger("Jump");
-        // this.ActionWaitTime(delay, () => {
-            switch (typeMove)
-            {
-                case EMovement.MoveToTop:
-                    _nextPosition += new Vector3(1f, 0f, 0f);
-                    _nextRotation = Quaternion.Euler(0f, 0f, 0f);
-                    return;
-                case EMovement.MoveToBottom:
-                    _nextPosition -= new Vector3(1f, 0f, 0f);
-                    _nextRotation = Quaternion.Euler(0f, 180f, 0f);
-                    return;
-                case EMovement.MoveToLeft:
-                    _nextPosition += new Vector3(0f, 0f, 1f);
-                    _nextRotation = Quaternion.Euler(0f, -90f, 0f);
-                    return;
-                case EMovement.MoveToRight:
-                    _nextPosition -= new Vector3(0f, 0f, 1f);
-                    _nextRotation = Quaternion.Euler(0f, 90f, 0f);
-                    return;
-            }
-        // });
+        this.ActionWaitTime(delay , () => {
+            
+        switch (typeMove)
+        {
+            case EMovement.MoveToTop:
+                _nextPosition += new Vector3(1f, 0f, 0f);
+                _nextRotation = Quaternion.Euler(0f, 0f, 0f);
+                return;
+            case EMovement.MoveToBottom:
+                _nextPosition -= new Vector3(1f, 0f, 0f);
+                _nextRotation = Quaternion.Euler(0f, 180f, 0f);
+                return;
+            case EMovement.MoveToLeft:
+                _nextPosition += new Vector3(0f, 0f, 1f);
+                _nextRotation = Quaternion.Euler(0f, -90f, 0f);
+                return;
+            case EMovement.MoveToRight:
+                _nextPosition -= new Vector3(0f, 0f, 1f);
+                _nextRotation = Quaternion.Euler(0f, 90f, 0f);
+                return;
+        }
+        });
     }
     private void AddListenInput()
     {
         if(Input.GetKeyDown(KeyCode.W)){
             Movement(EMovement.MoveToTop);   
+            Debug.LogWarning($"{_nextPosition}");
             return;
         }
         if(Input.GetKeyDown(KeyCode.S)){
             Movement(EMovement.MoveToBottom); 
+            Debug.LogWarning($"{_nextPosition}");
             return;
         }
         if(Input.GetKeyDown(KeyCode.A)){
             Movement(EMovement.MoveToLeft); 
+            Debug.LogWarning($"{_nextPosition}");
             return;
         }
         if(Input.GetKeyDown(KeyCode.D)){
             Movement(EMovement.MoveToRight); 
+            Debug.LogWarning($"{_nextPosition}");
             return;
         }
     }
